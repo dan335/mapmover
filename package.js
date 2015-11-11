537711cb41bc8c73d475226fd7215f5081b8e4e6
dan335 <danphi@gmail.com>
2015-11-11 09:22:02 -0800

    missing underscore dependency

--- package.js
+++ package.js
@@ -1,13 +1,16 @@
 Package.describe({
   name: 'danimal:mapmover',
   summary: 'Handle dragging and scaling of an element.',
-  version: '1.0.8',
+  version: '1.0.10',
   git: 'https://github.com/dan335/mapmover'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1');
-  api.use('reactive-var')
-  api.addFiles('mapmover.js');
+  api.use([
+    'reactive-var',
+    'underscore'
+  ])
+  api.addFiles('mapmover.js', 'client');
   api.export('Mapmover', 'client')
 });
